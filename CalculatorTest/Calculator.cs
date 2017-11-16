e6d5bca265017a3ac74cfada49eec8f667441879
amelzekovic <amelzekovic@gmail.com>
2017-11-16 23:06:53 +0100

    First commit

---
+++ CalculatorTest/Calculator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTest
+{
+    class Calculator
+    {
+        public Calculator(int operand1, int operand2)
+        {
+            this.operand1 = operand1;
+            this.operand2 = operand2;
+
+        }
+
+        public int operand1;
+        public int operand2;
+        const double pi = 3.14;
+        public void WriteOperands()
+        {
+            Console.WriteLine("Operand 1: " + this.operand1 + " Operand 2: " + this.operand2);
+        }
+
+        public int saberi()
+        {
+            return this.operand1 + this.operand2;
+        }
+
+        public int oduzmi()
+        {
+            return this.operand1 - this.operand2;
+        }
+
+        public int pomnozi()
+        {
+            return this.operand1 * this.operand2;
+        }
+
+        public double dijeli()
+        {
+            return this.operand1 / this.operand2;
+        }
+        public string WriteText(string text)
+        {
+            return text;
+        }
+
+        public  string WriteNumber(double number)
+        {
+            return number.ToString();
+        }
+
+        public static int saberi(int operand1, int operand2)
+        {
+            return operand1 + operand2;
+        }
+
+        public static int oduzmii(int operand1, int operand2)
+        {
+            return operand1 - operand2;
+        }
+
+        public static int pomnozi(int operand1, int operand2)
+        {
+            return operand1 * operand2;
+        }
+
+        public static double podijeli(int operand1, int operand2)
+        {
+            return operand1 / operand2;
+        }
+
+        public double povrsina_kruga()
+        {
+            return this.operand1 * this.operand1 * pi;
+        }
+
+    }   
+}
---
+++ CalculatorTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Unesi prvi broj:");
+            int operand1 = Int32.Parse (Console.ReadLine());
+
+            Console.WriteLine("Unesi drugi broj:");
+            int operand2 = Int32.Parse(Console.ReadLine());
+            Calculator c = new Calculator(operand1, operand2);
+            c.WriteOperands();
+
+            Console.WriteLine("Sabiranje");
+            string sabiranje = c.WriteNumber(c.saberi());
+            Console.WriteLine(sabiranje);
+            Console.ReadLine();
+
+            Console.WriteLine("Oduzimanje");
+            string oduzimanje = c.WriteNumber(c.oduzmi());
+            Console.WriteLine(oduzimanje);
+            Console.ReadLine();
+
+            Console.WriteLine("Pomnozi");
+            string mnozenje = c.WriteNumber(c.pomnozi());
+            Console.WriteLine(mnozenje);
+            Console.ReadLine();
+
+            Console.WriteLine("Podijeli");
+            string dijeljenje = c.WriteNumber(c.dijeli());
+            Console.WriteLine(dijeljenje);
+            Console.ReadLine();
+
+            Console.WriteLine("Povrsina kruga");
+            string povrsina = c.WriteNumber(c.povrsina_kruga());
+            Console.WriteLine(povrsina);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
